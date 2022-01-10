ec4a64245ba22c7d4dbfc99963dd69417da0190d
br0uun <jbr0uun@gmail.com>
2022-01-10 17:57:36 -0500

    FpsTutCommit
    FpsTutCommit

---
+++ FpsTutSec2/Assets/Scripts/BulletController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float moveSpeed, lifetime;
+
+    public Rigidbody theRB;
+
+    public GameObject impactEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = transform.forward * moveSpeed;
+
+        lifetime -= Time.deltaTime;
+
+        if(lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy"){
+            Destroy(other.gameObject);
+        }
+
+        Destroy(gameObject);
+        Instantiate(impactEffect, transform.position + (transform.forward * - moveSpeed * Time.deltaTime), transform.rotation);
+    }
+}
